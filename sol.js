abaf9795f2d4ee328b46929a6a7b2bf9ffc13f2d
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-09 14:55:51 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,4 +1,66 @@
-new Vue({
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Monster Slayer</title>
+    <script src="vue.js"></script>
+     <link rel="stylesheet" href="foundation.min.css">
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+<div id="app">
+    <section class="row">
+        <div class="small-6 columns">
+            <h1 class="text-center">YOU</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;" :style="{width: playerHealth + '%'}">
+                 
+                    {{playerHealth}}
+                </div>
+            </div>
+        </div>
+        <div class="small-6 columns">
+            <h1 class="text-center">MONSTER</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;" :style="{width: monsterHealth + '%'}">
+{{monsterHealth}}
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="row controls" v-if="!gameIsRunning">
+        <div class="small-12 columns">
+            <button id="start-game" @click="startGame">START NEW GAME</button>
+        </div>
+    </section>
+    <section class="row controls" v-else>
+        <div class="small-12 columns">
+            <button id="attack" @click="attack">ATTACK</button>
+            <button id="special-attack" @click="specialAttack">SPECIAL ATTACK</button>
+            <button id="heal" @click="heal">HEAL</button>
+            <button id="give-up" @click="giveUp">GIVE UP</button>
+        </div>
+    </section>
+    <section class="row log">
+        <div class="small-12 columns">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </section>
+</div>
+<script src="app.js"></script>
+</body>
+</html>
+
+
+
+
+-------------
+
+
+    new Vue({
     el:'#app',
     
     data:{
@@ -35,10 +97,18 @@ new Vue({
             this.checkWin()
         },
         heal:function(){
+            if(this.playerHealth <=90) {
+                this.playerHealth +=10
+            }else {
+                this.playerHealth=100
+            }
+            this.monsterAttack()
 
         },
         giveUp:function(){
-
+            this.gameIsRunning=false
+            this.playerHealth=100
+            this.monsterHealth=100
         },
         calculateDamage:function(min,max){
             return Math.max(Math.floor(Math.random() * max) +1,min)

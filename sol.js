83f227a4ede045b1334f9b7e5a33716a36ba09db
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-09 16:00:12 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,22 +1,43 @@
+var vm1 = new Vue({
+  el: "#app1",
+  data: {
+    title: "The Vue Instance",
+    showPara: false,
+  },
+  methods: {
+    show: function () {
+      this.showPara = true;
+      this.updateTitle("The new Instance (updated)");
+    },
+    updateTitle: function (title) {
+      this.title = title;
+    },
+  },
+  computed: {
+    lowerCaseTitle: function () {
+      return this.title.toLowerCase();
+    },
+  },
+  watch: {
+    title: function (value) {
+      alert("Title changed....!  " + value);
+    },
+  },
+});
+// changed by window fun
+setTimeout(() => {
+  vm1.title = "changed by timer";
+}, 3000);
+// another instance
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="vue.js"></script>
-</head>
-<body>
-
-    <div id="app">
-      <h1>{{title}}</h1>
-      <button @click="show">Show Para</button>
-      <p v-if="showPara">This is not always visible</p>
-    </div>
-
-<script src="app.js"></script>
-    
-    
-</body>
-</html>
+var vm2 = new Vue({
+  el: "#app2",
+  data: {
+    title: "The another Vue Instance",
+  },
+  methods: {
+    onChange: function () {
+      vm1.title = "changed by vm2";
+    },
+  },
+});

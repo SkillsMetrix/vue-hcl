69b1b29e0d88a20ca4bd91b64235b511459e6b8b
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-10 16:08:07 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,76 +1,21 @@
-Vue.component('hello',{
-    template :'<h3>template data </h3>'
-})
-var mydata= new Vue({
-    el:'#app',
-    data:{
-      title:'demo title'
+<template>
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      status: "Normal Working",
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "Better";
     },
-
-    beforeCreate() {
-       alert('before created')
-    },
-    created() {
-        alert('created')
-    },
-    beforeMount() {
-        alert('before mount')
-    },
-    mounted() {
-        alert('mounted')
-    },
-    beforeUpdate() {
-        console.log('before update');
-    },
-    updated() {
-        console.log('record updated');
-    },
-    beforeDestroy() {
-        alert('before destroy')
-    },
-    destroyed() {
-        alert('destroyed')
-    },
-    methods: {
-        destroy(){
-            this.$destroy()
-        }
-    },
-})
-
-
- -----
-
-   
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="vue.js"></script>
-</head>
-<body>
-
-    <div id="app">
-
-        <h1>{{title}}</h1>
-        <button @click="title='new title'">change</button>
-      <hello></hello>
-      <hello></hello>
-      <hello></hello>
-      <hello></hello>
-      <button @click="destroy">Destroy</button>
-    </div>
-
-
-    <script src="app.js"></script>
-    
-</body>
-</html>
+  },
+};
+</script>

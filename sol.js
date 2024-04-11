249be48126c2cad1476e10a5bc22cfd27c5cf0fc
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-11 16:32:38 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -2,18 +2,18 @@
   <div class="component">
     <p>User App Component</p>
 
-    <h3>{{ name }}</h3>
+    <h3>{{ name }} and age is {{ age }}</h3> 
 
     <button @click="changeName">Change Name</button>
 
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <UserDetails :name="name" @namewasreset="name=$event"></UserDetails>
+        <UserDetails :name="name" @namewasreset="name=$event" :userAge="age"></UserDetails>
       </div>
 
       <div class="col-xs-12 col-sm-6">
-        <UserEdit></UserEdit>
+        <UserEdit :userAge="age" @agewaschanged="age=$event"></UserEdit>
       </div>
     </div>
   </div>
@@ -25,7 +25,8 @@ import UserDetails from "./UserD.vue";
 export default {
   data() {
     return {
-      name:'Admin'
+      name:'Admin',
+      age:20
     }
   },
   methods: {
@@ -52,18 +53,56 @@ div {
 
   <template>
     <div class="component">
+        UserAge is : {{ userAge }}
+        <button @click="editAge">Change Age</button>
+    </div>
+ 
+</template>
+
+
+<script>
+import { eventBus } from "../main";
+export default {
+props :['userAge'],
+
+methods:{
+    editAge(){
+        this.userAge=40
+       // this.$emit('agewaschanged',this.userAge)
+       eventBus.$emit('agewaschanged',this.userAge)
+
+    }
+}
+}
+</script>
+
+<style scoped>
+div{
+    background-color: lightgreen;
+}
+</style>
+
+
+---------
+
+
+  <template>
+    <div class="component">
    USer Details
-   <p>User Name: {{ switchName() }}</p>
+   <p>User Name: {{ switchName() }} and age is : {{ userAge }}</p>
    <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
+
 export default{
     props:{
        name:{
         type:String
-       }
+       },
+       userAge: Number
     },
     methods: {
         switchName(){
@@ -74,6 +113,11 @@ export default{
             this.$emit('namewasreset',this.name)
         }
     },
+    created() {
+        eventBus.$on('agewaschanged',(age)=>{
+            this.userAge=age
+        })
+    },
 
 }
 </script>
@@ -83,3 +127,6 @@ div{
     background-color: lightcoral;
 }
 </style>
+
+
+----

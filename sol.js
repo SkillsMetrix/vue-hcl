0a7a35321fb7783e1ca4fdf17023f32b86d7a3b6
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-15 15:04:09 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,45 +1,28 @@
 <template>
-    <div>
-        <div
-                id="on"
-                @click="switched(true)"
-                :class="{active: value}">On</div>
-        <div
-                id="off"
-                @click="switched(false)"
-                :class="{active: !value}">Off</div>
-    </div>
+  <div>
+    <p>User Registration</p>
+    UserName:<input placeholder="Enter UserName" v-model="user.uname" />
+    <br />
+    Email:<input type="email" placeholder="Enter Email" v-model="user.email" />
+    <br />
+    <button @click="addUser">Add User</button>
+  </div>
 </template>
-
-<script>
+  <script>
     export default {
-        props: ['value'],
-        methods: {
-            switched(isOn) {
-                this.$emit('input', isOn);
-            }
-        }
-    }
-</script>
+      data() {
+        return {
+          user: {
+            uname: "",
+            email: "",
+          },
+        };
+      },
+      methods: {
+        addUser() {
+          console.log(this.user);
+        },
+      },
+    };
+  </script>
 
-<style scoped>
-    #on, #off {
-        width: 40px;
-        height: 20px;
-        background-color: lightgray;
-        padding: 2px;
-        display: inline-block;
-        margin: 10px -2px;
-        box-sizing: content-box;
-        cursor: pointer;
-        text-align: center;
-    }
-
-    #on:hover, #on.active {
-        background-color: lightgreen;
-    }
-
-    #off:hover, #off.active {
-        background-color: lightcoral;
-    }
-</style>

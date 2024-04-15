7675c495dd6eec28102f43dd1d834cf273b23cca
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-15 16:29:48 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -6,23 +6,50 @@
     Email:<input type="email" placeholder="Enter Email" v-model="user.email" />
     <br />
     <button @click="addUser">Add User</button>
+
+    <hr />
+    <div>
+      <p>User Details</p>
+      <ul>
+        <li v-for="u in udata">{{ u.uname }} --{{ u.email }}</li>
+      </ul>
+      <button @click="loadUsers">Load Users</button>
+    </div>
   </div>
 </template>
-  <script>
-    export default {
-      data() {
-        return {
-          user: {
-            uname: "",
-            email: "",
-          },
-        };
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        uname: "",
+        email: "",
       },
-      methods: {
-        addUser() {
-          console.log(this.user);
-        },
-      },
+      udata: [],
     };
-  </script>
-
+  },
+  methods: {
+    addUser() {
+      this.$http
+        .post("http://localhost:4000/adduser", this.user)
+        .then((response) => {
+          console.log(response);
+        });
+    },
+    loadUsers() {
+      this.$http
+        .get("http://localhost:4000/loadusers2")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const userArray = [];
+          for (let key in data) {
+            userArray.push(data[key]);
+          }
+          this.udata = userArray;
+        });
+    },
+  },
+};
+</script>

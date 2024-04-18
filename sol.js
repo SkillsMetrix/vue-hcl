98ef9afb7e11f699fbe50033db14dc75cfd854cb
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-18 13:37:49 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,61 +1,42 @@
 <template>
-  <div id="app">
-    <p>Routing Application</p>
-     
+    <div>
+        UserDetails component 
+        <p>User Loaded with ID :{{ $route.params.id }}</p>
+        <hr>
+        <router-link tag="button" class="btn btn-warning" :to="link" > Edit User</router-link>
 
-    <hr>
-    <router-view name="header-top"></router-view>
-    <transition name="slide" mode="out-in">
-      <router-view></router-view>
-    </transition>
-   
-    <router-view name="header-bottom"></router-view>
     </div>
-    
+   
 </template>
 
 <script>
-import Header from './Header.vue'
 export default {
-  components:{
-Header
-  },
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    data(){
+        return {
+            link:{
+                name: 'userEdit',
+                params:{
+                    id:$route.params.id
+                },
+                query:{
+                    locale:'en',q:100
+                },
+                hash: '#data'
+            }
+        }
     }
-  }
 }
 </script>
 
-<style>
-.slide-leave-active{
-  transition: opacity 1s ease;
-  opacity: 0;
-  animation: slide-out 1s ease-out forwards;
-}
-.slide-leave{
-  opacity: 0;
-  transform: translateX(0);
-}
-.slide-enter-active{
-  animation: slide-out 1s ease-out forwards;
-}
-@keyframes slide-out{
-  0%{
-    transform: translateY(0);
-  }
-  100%{
-    transform: translateY(-30px);
-  }
-}
-@keyframes slide-in {
-  0%{
-    transform: translateY(-30px);
-  }
-  100%{
-    transform: translateY(0);
-  }
-}
-</style>
+------------------
+  <template>
+    <div>
+        UserEdit component:
+        <p>Locale :{{$route.query.locale}}</p>
+        <p>Analytics :{{$route.query.q}}</p>
+
+        <div style="height: 700px;"></div>
+        <p id="data">Some extra data</p>
+    </div>
+   
+</template>

3b0d138bf7aa49864f6cf1e687abf529213e5fc9
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-18 14:11:48 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,32 +1,80 @@
 <template>
     <div>
-        UserEdit component:
-        <p>Locale :{{$route.query.locale}}</p>
-        <p>Analytics :{{$route.query.q}}</p>
-<hr>
-<button class="btn btn-primary" @click="confirmed=true">Confirm</button>
-        <div style="height: 700px;"></div>
-        <p id="data">Some extra data</p>
+        <h3>Some User Details</h3>
+        <p>User loaded has ID: {{ $route.params.id }}</p>
+        <router-link
+                tag="button"
+                :to="link"
+                class="btn btn-primary">Edit User
+        </router-link>
     </div>
-   
 </template>
+
 <script>
-export default {
-    data(){
-        return{
-            confirmed: false
-        }
-    },
-    beforeRouteLeave (to, from, next) {
-        if(this.confirmed){
-            next()
-        }else{
-            if(confirm('Are you sure?')){
-                next()
-            }else {
-                next(false)
+    export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            if (true) {
+                next();
+            } else {
+                next(false);
             }
         }
     }
-}
 </script>
+
+
+
+
+
+--------
+
+
+    <template>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.locale }}</p>
+        <p>Analytics: {{ $route.query.q }}</p>
+        <hr>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+        <div style="height: 700px"></div>
+        <p id="data">Some extra Data</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+              confirmed: false
+          }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (this.confirmed) {
+                next();
+            } else {
+                if (confirm('Are you sure?')) {
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
+    }
+</script>
+
+

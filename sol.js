87db9476e55db1c5ff8eeb5d36acb1caec95fed8
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-26 16:07:44 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,73 +1,33 @@
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&family=VT323&display=swap');
+<template>
+   <form @submit.prevent="handleSubmit">
+<input  placeholder="Enter Task" v-model="newTask"/>
+<button>Add Task</button>
+   </form>
+</template>
 
-body {
-  background: #f2f2f2;
-  color: #444;
-  margin: 0;
-}
-body * {
-  font-family: "Poppins";
-}
+<script>
+import { useTaskStore } from '@/stores/TaskStore';
+import { ref } from 'vue';
 
-/* header */
-header {
-  text-align: center;
-  background: #e7e7e7;
-  padding-top: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-header img {
-  max-width: 60px;
-  transform: rotate(-10deg);
-}
-header h1 {
-  margin: 0;
-  font-size: 2em;
-  padding-top: 25px;
-  margin-left: 15px;
-  color: #777;
-  transform: rotate(2deg);
-}
-.task-list{
-  max-width: 640px;
-  margin: 20px auto;
-}
-.task{
-  padding:6px 20px;
-  background: #fff;
-  margin-top: 20px;
-  border-radius: 4px;
-  box-shadow: 2 px 4px 6px rgba(0,0,0,0.05);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.task h3,.task .icons{
-  display: inline-block;
-}
-.task .icons{
-  text-align: right;
-}
-.task i{
-  font-size: 1.4em;
-  margin-left: 6px;
-  cursor: pointer;
-  color: #bbb;
-}
-.filter{
-  width: 640px;
-  margin: 10px auto;
-  text-align: right;
-}
-.filter button{
-  display: inline-block;
-  margin-left: 10px;
-  background: pink;
-  border: 2px solid #555;
-  border-radius: 4px;
-  padding: 4px 8px;
-  cursor: pointer;
-  font-size: 1em;
-}
+    export default {
+        setup(){
+            const taskStore=useTaskStore()
+            const newTask= ref('')
+            const handleSubmit=()=>{
+                if(newTask.value.length >0){
+                    taskStore.addTask({
+                        title:newTask.value,
+                        isFav:false,
+                        id:Math.floor(Math.random()* 10000)
+                    })
+                    newTask.value=''
+                }
+            }
+            return {handleSubmit,newTask}
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

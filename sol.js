87ba6e31ce9a618b2b1359d9a2ceeda50d9ca295
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-02 14:42:42 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,55 +1,89 @@
-
-
 import { shallowMount } from '@vue/test-utils'
-import Search from '@/components/Search.vue'
+import Weather from '@/components/Weather.vue'
 
-describe('Search Vue Specs',()=>{
-    let wrapper=null
 
-    beforeEach(()=>{
-        wrapper= shallowMount(Search)
+describe('Weather.vue Implementation Test', () => {
+  let wrapper = null
+
+  // SETUP - run before to each unit test
+  beforeEach(() => {
+    // render the component
+    wrapper = shallowMount(Weather, {
+      propsData: {
+        city: '',
+        weatherSummary: '',
+        weatherDescription: '',
+        currentTemperature: 0.0,
+        lowTemperature: 0.0,
+        highTemperature: 0.0
+      }
+    })
+  })
+
+  // TEARDOWN - run after to each unit test
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  it('initializes with correct elements', () => {
+    // check the name of the component
+    expect(wrapper.name()).toMatch('Weather')
+
+    // check that the heading text is rendered
+    expect(wrapper.findAll('h2').length).toEqual(2)
+    expect(wrapper.findAll('h2').at(0).text()).toMatch('Weather Summary')
+    expect(wrapper.findAll('h2').at(1).text()).toMatch('Temperatures')
+
+    // check that 6 fields of data for the temperature are displayed
+    expect(wrapper.findAll('p').length).toEqual(6)
+    expect(wrapper.findAll('p').at(0).text()).toMatch('City:')
+    expect(wrapper.findAll('p').at(1).text()).toMatch('Summary:')
+    expect(wrapper.findAll('p').at(2).text()).toMatch('Details:')
+    expect(wrapper.findAll('p').at(3).text()).toMatch('Current: 0° F')
+    expect(wrapper.findAll('p').at(4).text()).toMatch('High (Today): 0° F')
+    expect(wrapper.findAll('p').at(5).text()).toMatch('Low (Today): 0° F')
+  })
+
+  it('processes valid props data', () => {
+    // Update the props passed in to the Weather component
+    wrapper.setProps({
+      city: 'Chicago',
+      weatherSummary: 'Cloudy',
+      weatherDescription: 'Cloudy with a chance of rain',
+      currentTemperature: 45.1,
+      lowTemperature: 42.0,
+      highTemperature: 47.7
     })
 
-    afterEach(()=>{
-        wrapper.destroy()
-    })
+    // check that the prop data is stored as expected within the component
+    expect(wrapper.vm.city).toMatch('Chicago')
+    expect(wrapper.vm.weatherSummary).toMatch('Cloudy')
+    expect(wrapper.vm.weatherDescription).toMatch('Cloudy with a chance of rain')
+    expect(wrapper.vm.currentTemperature).toEqual(45.1)
+    expect(wrapper.vm.lowTemperature).toBeCloseTo(42.0)
+    expect(wrapper.vm.highTemperature).toBe(47.7)
 
-    it('check with elements',()=>{
-        expect(wrapper.name()).toMatch('Search')
-    })
+    // check that the heading text is rendered
+    expect(wrapper.findAll('h2').length).toEqual(2)
+    expect(wrapper.findAll('h2').at(0).text()).toMatch('Weather Summary')
+    expect(wrapper.findAll('h2').at(1).text()).toMatch('Temperatures')
 
-    it('check with h2',()=>{
-        expect(wrapper.findAll('h2').length).toEqual(1)
-        expect(wrapper.findAll('h2').at(0).text()).toMatch('Weather Search')
-    })
+    // check that 6 fields of data for the temperature are displayed
+    expect(wrapper.findAll('p').length).toEqual(6)
+    expect(wrapper.findAll('p').at(0).text()).toMatch('City: Chicago')
+    expect(wrapper.findAll('p').at(1).text()).toMatch('Summary: Cloudy')
+    expect(wrapper.findAll('p').at(2).text()).toMatch('Details: Cloudy with a chance of rain')
+    expect(wrapper.findAll('p').at(3).text()).toMatch('Current: 45.1° F')
+    expect(wrapper.findAll('p').at(4).text()).toMatch('High (Today): 47.7° F')
+    expect(wrapper.findAll('p').at(5).text()).toMatch('Low (Today): 42° F')
+  })
 
-    it('check with label',()=>{
-        expect(wrapper.findAll('h2').length).toEqual(1)
-        expect(wrapper.findAll('h2').at(0).text()).toMatch('Weather Search')
-    })
+  it('emits a custom event when the Clear Weather Data button is clicked', () => {
+    // trigger an event when the 'Clear Weather Data' button is clicked
+    wrapper.findAll('button').at(0).trigger('click')
 
-    it('check with label value',()=>{
-        expect(wrapper.findAll('label').length).toEqual(1)
-        expect(wrapper.findAll('label').at(0).text()).toMatch('City:')
-    })
-
-    it('check with buttons value',()=>{
-        expect(wrapper.findAll('button').length).toEqual(2)
-        expect(wrapper.findAll('button').at(0).text()).toMatch('Search')
-        expect(wrapper.findAll('button').at(1).text()).toMatch('Clear')
-        expect(wrapper.findAll('button').at(0).element.disabled).toBeTruthy
-        expect(wrapper.findAll('button').at(1).element.disabled).toBeTruthy
-
-
-
-    })
-    
-    it('check text value',()=>{
-       wrapper.setData({inputCity:'mumbai'})
-       wrapper.vm.searchCity()
-       expect(wrapper.emitted('search-city')[0][0]).toMatch('mumbai')
-       expect(wrapper.vm.inputCity).toMatch('mumbai')
-    })
-
+    // check that 1 occurrence of the event has been emitted
+    expect(wrapper.emitted('clear-weather-data')).toBeTruthy()
+    expect(wrapper.emitted('clear-weather-data').length).toBe(1)
+  })
 })
- 
